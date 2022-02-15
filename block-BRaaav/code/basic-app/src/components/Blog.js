07db9c31fea5c4d7e7c337e321882083a0a01160
Hayley Wood <onlinecowork@gmail.com>
2022-02-16 01:33:43 +1000

    React add data from array

--- block-BRaaav/code/basic-app/src/components/Blog.js
+++ block-BRaaav/code/basic-app/src/components/Blog.js
@@ -1,4 +1,32 @@
 import { FaClock } from "react-icons/fa";
+import Article from "./Article";
+
+let articleData = [
+  {
+    date: "December 22, 2017",
+    title: "How to find the perfect workspace",
+    description: "lorem ipsum lorem ipsum lorem ipsum",
+    buttonText: "Read Now",
+  },
+  {
+    date: "December 22, 2017",
+    title: "How to find the perfect workspace",
+    description: "lorem ipsum lorem ipsum lorem ipsum",
+    buttonText: "Read Article",
+  },
+  {
+    date: "December 22, 2017",
+    title: "How to find the perfect workspace",
+    description: "lorem ipsum lorem ipsum lorem ipsum",
+    buttonText: "Read Article",
+  },
+  {
+    date: "December 22, 2017",
+    title: "How to find the perfect workspace",
+    description: "lorem ipsum lorem ipsum lorem ipsum",
+    buttonText: "Read Article",
+  },
+];
 
 function Blog() {
   return (
@@ -14,90 +42,16 @@ function Blog() {
             </div>
           </header>
           <div className='flex wrap'>
-            <article className='flex article flex-48'>
-              <div className='flex-40 font-0'>
-                <img
-                  className='flexible-img'
-                  src='images/blog-image1.jpg'
-                  alt='Blog image 1'
+            {articleData.map((article) => {
+              return (
+                <Article
+                  date={article.date}
+                  title={article.title}
+                  description={article.description}
+                  buttonText={article.buttonText}
                 />
-              </div>
-
-              <div className='flex-60 article-content'>
-                <time datetime=''>
-                  <FaClock />
-                  &nbsp;December 22, 2017
-                </time>
-                <h3>How to find beautiful workspace?</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                <a className='btn btn-tertiary' href='#1'>
-                  Read Article
-                </a>
-              </div>
-            </article>
-            <article className='flex article flex-48'>
-              <div className='flex-40 font-0'>
-                <img
-                  className='flexible-img'
-                  src='images/blog-image2.jpg'
-                  alt='Blog image 1'
-                />
-              </div>
-
-              <div className='flex-60 article-content'>
-                <time datetime=''>
-                  <FaClock />
-                  &nbsp;December 18, 2017
-                </time>
-                <h3>Woman sportwear</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                <a className='btn btn-tertiary' href='#1'>
-                  Read More
-                </a>
-              </div>
-            </article>
-            <article className='flex article flex-48'>
-              <div className='flex-40 font-0'>
-                <img
-                  className='flexible-img'
-                  src='images/blog-image3.jpg'
-                  alt='Blog image 1'
-                />
-              </div>
-
-              <div className='flex-60 article-content'>
-                <time datetime=''>
-                  <FaClock />
-                  &nbsp;December 14, 2017
-                </time>
-                <h3>New creative fashion</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                <a className='btn btn-tertiary' href='#1'>
-                  Read Article
-                </a>
-              </div>
-            </article>
-            <article className='flex article flex-48'>
-              <div className='flex-40 font-0'>
-                <img
-                  className='flexible-img'
-                  src='images/blog-image4.jpg'
-                  alt='Blog image 1'
-                />
-              </div>
-
-              <div className='flex-60 article-content'>
-                <time datetime=''>
-                  <FaClock />
-                  &nbsp;December 10, 2017
-                </time>
-                <h3>Minimalist design trend in 2018</h3>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                <a className='btn btn-tertiary' href='#1'>
-                  View Detail
-                </a>
-              </div>
-            </article>
+              );
+            })}
           </div>
         </div>
       </section>
